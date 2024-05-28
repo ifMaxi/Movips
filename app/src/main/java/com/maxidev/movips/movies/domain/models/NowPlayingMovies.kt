11f96c16ab4afbfb08f6dfcffc52e1b5b1744d72
Maxi Pelizzoni <maxi96p@gmail.com>
2024-05-28 11:35:35 -0300

    Removed unnecessary data models.

--- app/src/main/java/com/maxidev/movips/movies/domain/models/NowPlayingMovies.kt
+++
@@ -1,8 +0,0 @@
-package com.maxidev.movips.movies.domain.models
-
-data class NowPlayingMovies(
-    val id: Int,
-    val title: String,
-    val posterPath: String,
-    val backdropPath: String
-)
--- app/src/main/java/com/maxidev/movips/movies/domain/models/TopRatedMovies.kt
+++
@@ -1,8 +0,0 @@
-package com.maxidev.movips.movies.domain.models
-
-data class TopRatedMovies(
-    val id: Int,
-    val title: String,
-    val posterPath: String,
-    val voteAverage: Double
-)
