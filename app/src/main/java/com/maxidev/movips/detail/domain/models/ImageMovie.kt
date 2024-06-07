e83c83f2f084b83c1967e08ed86776649d3cdc80
Maxi Pelizzoni <maxi96p@gmail.com>
2024-06-07 18:10:28 -0300

    Unnecessary components and models were removed.

--- app/src/main/java/com/maxidev/movips/detail/domain/models/ImageMovie.kt
+++
@@ -1,6 +0,0 @@
-package com.maxidev.movips.detail.domain.models
-
-data class ImageMovie(
-    val id: Int,
-    val filePath: String
-)
--- app/src/main/java/com/maxidev/movips/detail/presentation/state/ImageState.kt
+++
@@ -1,8 +0,0 @@
-package com.maxidev.movips.detail.presentation.state
-
-import com.maxidev.movips.detail.domain.models.ImageMovie
-
-sealed interface ImageState {
-    data class Success(val onSuccess: List<ImageMovie>): ImageState
-    data class Error(val onError: Exception): ImageState
-}
