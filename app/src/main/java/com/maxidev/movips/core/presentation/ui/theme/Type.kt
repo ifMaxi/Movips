5a4f2ff4b6eb0d67bd16d95d663e4e94e2004833
Maxi Pelizzoni <maxi96p@gmail.com>
2024-06-12 19:14:45 -0300

    Colors added.

--- app/src/main/java/com/maxidev/movips/core/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/maxidev/movips/core/presentation/ui/theme/Type.kt
@@ -8,18 +8,27 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.maxidev.movips.R
 
-val montserrat = FontFamily(
-    Font(resId = R.font.montserrat_light, weight = FontWeight.Light),
-    Font(resId = R.font.montserrat, weight = FontWeight.Normal),
-    Font(resId = R.font.montserrat_medium, weight = FontWeight.Medium),
-    Font(resId = R.font.montserrat_semibold, weight = FontWeight.SemiBold),
-    Font(resId = R.font.montserrat_bold, weight = FontWeight.Bold)
+val roboto = FontFamily(
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light),
+    Font(resId = R.font.roboto, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium)
+)
+
+val oswald = FontFamily(
+    Font(resId = R.font.oswald, weight = FontWeight.Normal),
+    Font(resId = R.font.oswald_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.oswald_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.oswald_semibold, weight = FontWeight.SemiBold)
+)
+
+val bebasNeue = FontFamily(
+    Font(resId = R.font.bebas_neue, weight = FontWeight.Normal)
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = montserrat,
+        fontFamily = roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
